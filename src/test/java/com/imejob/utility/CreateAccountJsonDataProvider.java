99537b2a38e2f872604d4129284ad61c764e7f5d
mohammed-sumairkhan <sumairn1432@gmail.com>
2025-09-07 17:32:42 +0530

    ADD DataProvider Class

---
+++ src/test/java/com/imejob/utility/CreateAccountJsonDataProvider.java
@@ -0,0 +1,41 @@
+package com.imejob.utility;
+
+import java.util.stream.StreamSupport;
+
+import org.testng.annotations.DataProvider;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import utility.CreateAccountJsonReader;
+
+public class CreateAccountJsonDataProvider {
+	
+	@DataProvider(name = "getCreateAccountData")
+    public static Object[][] getCreateAccountData() {
+        CreateAccountJsonReader reader = new CreateAccountJsonReader();
+        reader.loadJson("createAccountData"); // JSON file name without .json
+
+        // Get technicalSkills as array
+        JsonNode skillsNode = reader.getJsonNode("createAccount", "technicalSkills");
+        String[] technicalSkills = StreamSupport.stream(skillsNode.spliterator(), false)
+                                                .map(JsonNode::asText)
+                                                .toArray(String[]::new);
+
+        return new Object[][]{
+            {
+                reader.getValue("createAccount", "firstName"),
+                reader.getValue("createAccount", "middleName"),
+                reader.getValue("createAccount", "lastName"),
+                reader.getValue("createAccount", "email"),
+                reader.getValue("createAccount", "phoneNumber"),
+                reader.getValue("createAccount", "totalExperience"),
+                reader.getValue("createAccount", "currentLocation"),
+                reader.getValue("createAccount", "password"),
+                reader.getValue("createAccount", "gender"),
+                technicalSkills, // Pass array directly
+                reader.getValue("createAccount", "resumePath"),
+                Boolean.parseBoolean(reader.getValue("createAccount", "agreeTerms"))
+            }
+        };
+    }
+}
