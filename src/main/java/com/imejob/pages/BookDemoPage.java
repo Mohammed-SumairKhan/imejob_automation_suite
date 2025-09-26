037fb014c5a8abb3816bd2fa0f99aeec4c67b781
Mohammed-SumairKhan <sumairn1432@gmail.com>
2025-09-26 18:09:11 +0530

    updated

--- src/main/java/com/imejob/pages/BookDemoPage.java
+++ src/main/java/com/imejob/pages/BookDemoPage.java
@@ -10,53 +10,57 @@ import utility.WaitUtils;
 
 public class BookDemoPage {
 	WebDriver driver;
-	
+
 	@FindBy(xpath = "//button[text() = 'Book a demo']")
 	WebElement bookDemo;
-	
+
 	@FindBy(name = "email")
 	WebElement emailInput;
-	
+
 	@FindBy(name = "phoneNumber")
 	WebElement phoneNumberInput;
-	
+
 	@FindBy(name = "firstName")
 	WebElement firstNameInput;
-	
+
 	@FindBy(name = "lastName")
 	WebElement lastNameInput;
-	
+
 	@FindBy(name = "message")
 	WebElement messageInput;
-	
+
 	@FindBy(xpath = "//button[text() = 'Submit']")
 	WebElement submitButton;
-	
+
 	@FindBy(xpath = "//div[contains(@class, 'indicatorContainer')]")
 	WebElement dropDown;
-	
+
+	@FindBy(xpath = "//*[contains(text(),'Thanks for contacting us!')]")
+	private WebElement successMessage;
+
 	public BookDemoPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public void clickBookDemo() {
 		try {
 			bookDemo.click();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException("cann't click the book demo button " + e);
 		}
 	}
-	
+
 	public void clickSubmitButton() {
 		try {
 			submitButton.click();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException("cann't click the submit button " + e);
 		}
 	}
-	public void fillContactForm(String email, String phoneNumber, String firstName, String lastName,
-			String subject, String message) {
+
+	public void fillContactForm(String email, String phoneNumber, String firstName, String lastName, String subject,
+			String message) {
 		emailInput.sendKeys(email);
 		phoneNumberInput.sendKeys(phoneNumber);
 		firstNameInput.sendKeys(firstName);
@@ -64,23 +68,20 @@ public class BookDemoPage {
 		selectSubject(subject);
 		messageInput.sendKeys(message);
 	}
-	
+
 	public void selectSubject(String subject) {
-		
+
 		dropDown.click();
-		//div[text() = 'Looking for Dedicated HR Services']
-	
-		 WebElement options= driver.findElement(By.xpath("//div[text() = '"+subject+"']"));
+		// div[text() = 'Looking for Dedicated HR Services']
+
+		WebElement options = driver.findElement(By.xpath("//div[text() = '" + subject + "']"));
 		WaitUtils.waitForElementClickable(driver, options);
-		 options.click();
-		  
+		options.click();
+
 	}
- }
 
-
-
-
-
-
-
-
+	public String getStatusMessage() {
+		WaitUtils.waitForElementVisible(driver, successMessage);
+		return successMessage.getText();
+	}
+}
